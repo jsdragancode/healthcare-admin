8bf014c210a8d033ba0f10e645bf6b79ffc0030e
echoLive <superguy0613519@gmail.com>
2020-12-10 05:30:47 -0800

    Update default url to login page

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ ReactDOM.render(
       <Route path="/rtl" component={RtlLayout} />
       <Route path="/auth" component={AuthLayout} />
       <Route path="/admin" component={AdminLayout} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/" to="/auth/login" />
     </Switch>
   </Router>,
   document.getElementById('root')
