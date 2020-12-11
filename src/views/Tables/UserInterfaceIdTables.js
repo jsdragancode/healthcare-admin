256b60173c6324475cb177ee172557742a783c79
echoLive <superguy0613519@gmail.com>
2020-12-11 12:46:44 -0800

    Add message function based on actions on UserInterfaceId Page

--- src/views/Tables/UserInterfaceIdTables.js
+++ src/views/Tables/UserInterfaceIdTables.js
@@ -25,6 +25,7 @@ import CardHeader from 'components/Card/CardHeader.js';
 import ReactTableBottomPagination from '../../components/ReactTableBottomPagination/ReactTableBottomPagination.js';
 
 import { cardTitle } from '../../assets/jss/material-dashboard-pro-react.js';
+import Snackbar from "../../components/Snackbar/Snackbar.js";
 
 const styles = {
     cardIconTitle: {
@@ -51,6 +52,10 @@ export default function UserInterfaceTables() {
     const [newDeviceOS, setNewDeviceOS] = useState('');
     const [newDeviceModel, setNewDeviceModel] = useState('');
     const classes = useStyles();
+    const [failed, setFailed] = React.useState(false);
+    const [success, setSuccess] = React.useState(false);
+    const [updateFailed, setUpdateFailed] = React.useState(false);
+    const [updateSuccess, setUpdateSuccess] = React.useState(false);
 
     const setUserInterface = (info) => {
         const { user_id, instance_id, device_os, device_model } = info;
@@ -124,6 +129,17 @@ export default function UserInterfaceTables() {
             .then((res) => {
                 setData([...data, makeTableRow(res.data.userInterface)]);
                 setAddModal(false);
+                setSuccess(true);
+                setTimeout(function () {
+                    setSuccess(false);
+                }, 3000);
+            })
+            .catch((e) => {
+                console.log(e);
+                setFailed(true);
+                setTimeout(function () {
+                    setFailed(false);
+                }, 3000);
             });
     };
 
@@ -142,6 +158,18 @@ export default function UserInterfaceTables() {
                     )
                 );
                 setEditModal(false);
+
+                setUpdateSuccess(true);
+                setTimeout(function () {
+                    setUpdateSuccess(false);
+                }, 3000);
+            })
+            .catch((e) => {
+                console.log(e);
+                setUpdateFailed(true);
+                setTimeout(function () {
+                    setUpdateFailed(false);
+                }, 3000);
             });
     };
 
@@ -409,6 +437,42 @@ export default function UserInterfaceTables() {
                 </Button>
                             </DialogActions>
                         </Dialog>
+                        <Snackbar
+                            place="tr"
+                            color="success"
+                            // icon={AddAlert}
+                            message="Your new data was added successfully."
+                            open={success}
+                            closeNotification={() => setSuccess(false)}
+                            close
+                        />
+                        <Snackbar
+                            place="tr"
+                            color="rose"
+                            // icon={AddAlert}
+                            message="Failed to add new data. Please try again."
+                            open={failed}
+                            closeNotification={() => setFailed(false)}
+                            close
+                        />
+                        <Snackbar
+                            place="tr"
+                            color="success"
+                            // icon={AddAlert}
+                            message="Your new data was updated successfully."
+                            open={updateSuccess}
+                            closeNotification={() => setUpdateSuccess(false)}
+                            close
+                        />
+                        <Snackbar
+                            place="tr"
+                            color="rose"
+                            // icon={AddAlert}
+                            message="Failed to update new data. Please try again."
+                            open={updateFailed}
+                            closeNotification={() => setUpdateFailed(false)}
+                            close
+                        />
                     </CardBody>
                 </Card>
             </GridItem>
