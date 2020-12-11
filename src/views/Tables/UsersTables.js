6847321d93e182e4cddf9ecb5032cc5d7fe2bdd1
echoLive <superguy0613519@gmail.com>
2020-12-11 12:44:32 -0800

    Add message function based on actions on Users Page

--- src/views/Tables/UsersTables.js
+++ src/views/Tables/UsersTables.js
@@ -39,6 +39,8 @@ import { cardTitle } from '../../assets/jss/material-dashboard-pro-react.js';
 import customSelectStyle from '../../assets/jss/material-dashboard-pro-react/customSelectStyle.js';
 import customCheckboxRadioSwitch from 'assets/jss/material-dashboard-pro-react/customCheckboxRadioSwitch.js';
 
+import Snackbar from "../../components/Snackbar/Snackbar.js";
+
 const styles = {
   ...customSelectStyle,
   ...customCheckboxRadioSwitch,
@@ -75,6 +77,10 @@ export default function UsersTables() {
   const [registeredOn, setRegisteredOn] = useState('');
   const [checkedA, setCheckedA] = React.useState(true);
   const [newFullNameAr, setNewFullNameAr] = useState('');
+  const [failed, setFailed] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  const [updateFailed, setUpdateFailed] = React.useState(false);
+  const [updateSuccess, setUpdateSuccess] = React.useState(false);
 
   const classes = useStyles();
 
@@ -190,6 +196,17 @@ export default function UsersTables() {
           registered_on: '',
         });
         setAddModal(false);
+        setSuccess(true);
+        setTimeout(function () {
+          setSuccess(false);
+        }, 3000);
+      })
+      .catch((e) => {
+        console.log(e);
+        setFailed(true);
+        setTimeout(function () {
+          setFailed(false);
+        }, 3000);
       });
   };
 
@@ -215,6 +232,18 @@ export default function UsersTables() {
         );
         // setUserParam('', '', '');
         setEditModal(false);
+
+        setUpdateSuccess(true);
+        setTimeout(function () {
+          setUpdateSuccess(false);
+        }, 3000);
+      })
+      .catch((e) => {
+        console.log(e);
+        setUpdateFailed(true);
+        setTimeout(function () {
+          setUpdateFailed(false);
+        }, 3000);
       });
   };
 
@@ -702,6 +731,42 @@ export default function UsersTables() {
                 </Button>
               </DialogActions>
             </Dialog>
+            <Snackbar
+              place="tr"
+              color="success"
+              // icon={AddAlert}
+              message="Your new data was added successfully."
+              open={success}
+              closeNotification={() => setSuccess(false)}
+              close
+            />
+            <Snackbar
+              place="tr"
+              color="rose"
+              // icon={AddAlert}
+              message="Failed to add new data. Please try again."
+              open={failed}
+              closeNotification={() => setFailed(false)}
+              close
+            />
+            <Snackbar
+              place="tr"
+              color="success"
+              // icon={AddAlert}
+              message="Your new data was updated successfully."
+              open={updateSuccess}
+              closeNotification={() => setUpdateSuccess(false)}
+              close
+            />
+            <Snackbar
+              place="tr"
+              color="rose"
+              // icon={AddAlert}
+              message="Failed to update new data. Please try again."
+              open={updateFailed}
+              closeNotification={() => setUpdateFailed(false)}
+              close
+            />
           </CardBody>
         </Card>
       </GridItem>
